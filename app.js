1281d928b6e26b43c1cbf7f422b471c662c4ba53
Sivaraj V <sivaraj.v@EF.com>
2018-03-22 14:16:13 +0530

    Firefox Meta issue are updated

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
-[1, 2, 3].map(n => console.log(n));
 console.log(x); // 1
 console.log(y); // 2
 console.log(z); // { a: 3, b: 4 }
+[5, 6].map(n => console.log(n));
